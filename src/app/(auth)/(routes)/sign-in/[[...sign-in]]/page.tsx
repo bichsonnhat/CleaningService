8c82a4ac0b35a4901d9ff5041a8ced18f541911c
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-08-27 15:45:55 +0700

    feat: impl authentication

---
+++ src/app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignIn } from '@clerk/nextjs'
+
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <SignIn />
+    </div>
+  )
+}
+
+export default SignInPage
