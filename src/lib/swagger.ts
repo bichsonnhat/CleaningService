9b39fa2e196303eafec4de1e111b2e1e3c7f41c9
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-09-07 15:37:32 +0700

    chore(swagger.ts): change apiFolder path

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -2,7 +2,7 @@ import { createSwaggerSpec } from "next-swagger-doc";
 
 export const getApiDocs = async () => {
   const spec = createSwaggerSpec({
-    apiFolder: "src/app/(api)/(routes)/api/hello", // define api folder under app folder
+    apiFolder: "src/app/(api)/(routes)/api", // define api folder under app folder
     definition: {
       openapi: "3.0.0",
       info: {
