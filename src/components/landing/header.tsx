b4618e8d4e982162e72ced5e628b4a3faeb7c531
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-09-12 11:43:06 +0700

    feat: add custom GilroyFont

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,11 +1,27 @@
 import Link from 'next/link'
 import React from 'react'
+import MonitoLogo from '../img/logo'
+import localFont from 'next/font/local'
+import { Poppins } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import { Font } from '@react-pdf/renderer';
+
+const poppins = Poppins({
+  weight: "700",
+  subsets: ["latin"]
+})
+
+Font.register({
+  family: "FontName",
+  src: "/fonts/Super-Feel.ttf",
+});
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <div className='flex flex-row gap-[48px] justify-center items-center py-[28px]'>
+      <MonitoLogo/>
+      <Link href={'/dashboard'} className={cn("text-[#103559] font-gilroy-regular")} >Home</Link>
+    </div>  
   )
 }
 
