bff0b17c8978f24af4461d38fc310cf58ffb666f
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-09-28 00:26:35 +0700

    chore: remove unused currency/button components

--- src/components/currency/DropdownArrow.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DropdownArrow: React.FC = () => {
-  return (
-    <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/986f777c5c2d7842fb957fe037af381a179a419219749ec56110b731cddda13f?placeholderIfAbsent=true&apiKey=8d1a3048ff904e58908bc27809a01dd6" alt="" className="object-contain shrink-0 self-stretch my-auto w-6 aspect-square" />
-  );
-};
-
-export default DropdownArrow;
