70013d7aa339030ac1d5f4e966a232935696d939
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-09-29 17:47:34 +0700

    refactor: optimize header layout and update login button style

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,16 +4,16 @@ import React from 'react'
 const Header = () => {
   const links = ['Residential', 'Office', 'Commercial', 'FAQ\'s'];
   return (
-    <header className='flex justify-center w-full bg-transparent'>
-      <div className='flex flex-row w-full max-w-[1170px] justify-between items-end mt-[20px]'>
-        <img src='/img/Logo.svg' alt='Clean' className='h-8' /> {/* Adjust height as needed */}
-        <nav className='flex gap-[31px] items-end'>
+    <header className='flex justify-center bg-transparent'>
+      <div className='flex flex-row w-full max-w-[1170px] h-[38px] justify-between items-end mt-[20px]'>
+        <img src='/img/Logo.svg' alt='Clean' className='h-[38px]' /> 
+        <nav className='flex flex-wrap gap-8 items-center'>
           {links.map((link) => (
-            <a href={`#${link.toLowerCase()}`} key={link} className='text-gray-700 hover:text-gray-900 py-3 font-Averta-Semibold'>
+            <a href={`#${link.toLowerCase()}`} key={link} className='text-gray-700 font-Averta-Semibold mt-1'>
               {link}
             </a>
           ))}
-          <button className=" text-center text-blue-600 rounded-xl border-[3px] px-6 py-2 border-blue-600 border-solid font-Averta-Semibold">
+          <button className=" text-center text-blue-600 rounded-xl border-[3px] px-6 py-1 border-blue-600 border-solid font-Averta-Semibold">
             Login
           </button>
         </nav>
