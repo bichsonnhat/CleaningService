c53b26bc3a733ceef083802fb6f6616fafa92407
GiangPhanNguyenTra <22520360@gm.uit.edu.vn>
2024-10-25 16:52:42 +0700

    fix: adjust table

--- src/components/customer/CustomerRow.tsx
+++ src/components/customer/CustomerRow.tsx
@@ -16,44 +16,44 @@ const CustomerRow: React.FC<CustomerRowProps> = ({
   email,
 }) => {
   return (
-    <div className="flex flex-wrap gap-3 w-full border-b border-gray-200 bg-white hover:bg-[#f4f7ff] h-auto md:h-[80px] items-start md:items-center p-2.5 cursor-pointer">
-      <div className="w-full md:w-[80px] flex items-center justify-start  md:pl-0 mb-2 md:mb-0">
-        <div className="text-sm text-[#202224] font-Averta-Semibold">
+    <div className="flex flex-wrap gap-3 w-full border-b border-gray-200 bg-white hover:bg-[#f4f7ff] h-auto items-start md:items-center p-2.5 cursor-pointer">
+      <div className="w-full md:w-[8%] flex items-center justify-start md:py-6 mb-2 md:mb-0">
+        <div className="text-sm text-[#202224] font-semibold">
           <span className="md:hidden font-bold">ID: </span>
           {id}
         </div>
       </div>
 
-      <div className="w-full md:w-[177px] flex items-center justify-start md:pl-0 mb-2 md:mb-0">
-        <div className="text-sm text-[#202224] font-Averta-Semibold">
+      <div className="w-full md:w-[20%] flex items-center justify-start md:py-6 mb-2 md:mb-0">
+        <div className="text-sm text-[#202224] font-semibold">
           <span className="md:hidden font-bold">NAME: </span>
           {name}
         </div>
       </div>
 
-      <div className="w-full md:w-[342px] flex items-center justify-start  md:pl-0 mb-2 md:mb-0">
-        <div className="text-sm text-[#202224] font-Averta-Semibold">
+      <div className="w-full md:w-[26%] flex items-center justify-start md:py-6 mb-2 md:mb-0">
+        <div className="text-sm text-[#202224] font-semibold">
           <span className="md:hidden font-bold">ADDRESS: </span>
           {address}
         </div>
       </div>
 
-      <div className="w-full md:w-[170px] flex items-center justify-start md:pl-0 mb-2 md:mb-0">
-        <div className="text-sm text-[#202224cc] font-Averta-Regular">
+      <div className="w-full md:w-[11%] flex items-center justify-start md:py-6 mb-2 md:mb-0">
+        <div className="text-sm text-[#202224cc]">
           <span className="md:hidden font-bold">PHONE: </span>
           {phone}
         </div>
       </div>
 
-      <div className="w-full md:w-[250px] flex items-center justify-start md:pl-0 mb-2 md:mb-0">
-        <div className="text-sm text-[#202224cc] font-Averta-Regular truncate">
+      <div className="w-full md:w-[20%] flex items-center justify-start md:py-6 mb-2 md:mb-0">
+        <div className="text-sm text-[#202224cc] truncate">
           <span className="md:hidden font-bold">EMAIL: </span>
           {email}
         </div>
       </div>
 
-      <div className="w-full md:w-[100px] flex items-center justify-end md:pl-0">
-        <button className="px-4 py-1.5 bg-[#6896d1] text-[#12153a] bg-opacity-20 text-xs rounded-[4.5px] font-Averta-Semibold hover:bg-opacity-50">
+      <div className="w-full  md:w-[10%] flex items-center md:py-6">
+        <button className="ml-auto px-4 py-1.5 bg-[#6896d1] text-[#12153a] bg-opacity-20 text-xs rounded-[4.5px] font-semibold hover:bg-opacity-50">
           More Info
         </button>
       </div>
