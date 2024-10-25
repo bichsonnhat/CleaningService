9f32dce1cf1a0e5571ba533a67f62aecd7fc5163
GiangPhanNguyenTra <22520360@gm.uit.edu.vn>
2024-10-25 10:09:16 +0700

    feat: add dashboard/employee page

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,15 +1,13 @@
-import Header from '@/components/dashboard/Header'
-import Sidebar from '@/components/dashboard/Sidebar'
-import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom';
+import Header from "@/components/dashboard/Header";
+import Sidebar from "@/components/dashboard/Sidebar";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const DashboardLayout = (props: {
-    children: React.ReactNode
-}) => {
+const DashboardLayout = (props: { children: React.ReactNode }) => {
   return (
     <div className="flex overflow-hidden flex-col bg-slate-100">
       <Header />
-      <div className="flex gap-5 max-md:flex-col">
+      {/* <div className="flex gap-5 max-md:flex-col">
         <Sidebar />
         <main className="flex flex-col ml-5 w-[84%] max-md:ml-0 max-md:w-full">
           <div className="flex overflow-hidden flex-col grow items-center px-8 pt-5 pb-40 w-full bg-slate-100 min-h-[970px] max-md:px-5 max-md:pb-24 max-md:max-w-full">
@@ -18,9 +16,16 @@ const DashboardLayout = (props: {
             </div>
           </div>
         </main>
+      </div> */}
+
+      <div className="flex max-md:flex-col">
+        <Sidebar />
+        <main className="flex p-8 flex-col w-full max-md:ml-0 max-md:w-full">
+          {props.children}
+        </main>
       </div>
     </div>
-  )
-} 
+  );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
