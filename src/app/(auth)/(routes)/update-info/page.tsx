6ef33ac2d90cf95f038c422a42b31f1302ce1974
Tkhangds <gunnyvippro111@gmail.com>
2024-10-26 15:57:21 +0700

    feat: Add update info page
    - Added a new file `page.tsx` under `src/app/(auth)/(routes)/update-info` to implement the update info page.
    - Imported `UpdateCustomerInfo` and `UpdateStaffInfo` components from `@/components/updateinfo/customer/UpdateCustomerInfo` and `@/components/updateinfo/staff/UpdateStaffInfo` respectively.
    - Created a functional component `page` that renders the `UpdateStaffInfo` component.
    - Exported the `page` component as the default export.
    
    chore(deps): Update package dependencies
    
    - Updated the following dependencies in `package.json`:
      - `@radix-ui/react-checkbox` to version `^1.1.2`
      - `@radix-ui/react-icons` to version `^1.3.0`
      - `@radix-ui/react-label` to version `^2.1.0`
      - `@radix-ui/react-scroll-area` to version `^1.2.0`
      - `@radix-ui/react-select` to version `^2.1.2`
      - `@radix-ui/react-separator` to version `^1.1.0`
      - `react-router-dom` to version `^6.27.0`
    
    feat: Add upload.svg image
    
    - Added a new file `upload.svg` under `public/images` to use as an upload icon.
    
    refactor: Update ToggleButton component
    
    - Updated the `ToggleButton` component in `src/components/button/togglebutton.tsx` to improve readability and maintainability.
    - Replaced the `className` prop with individual class names for better styling.
    - Refactored the `handleToggle` function to use the `setIsToggled` hook.
    
    refactor: Update InputWithLabel component
    
    - Updated the `InputWithLabel` component in `src/components/input/inputwithlabel.tsx` to improve readability and maintainability.
    - Added support for a combobox input type with options.
    - Added the `plusPX` prop to adjust the width of the input element.
    - Updated the styling of the input element.
    
    feat: Add camera.svg image
    
    - Added a new file `camera.svg` under `public/images` to use as a camera icon.

---
+++ src/app/(auth)/(routes)/update-info/page.tsx
@@ -0,0 +1,9 @@
+import UpdateCustomerInfo from "@/components/updateinfo/customer/UpdateCustomerInfo";
+import UpdateStaffInfo from "@/components/updateinfo/staff/UpdateStaffInfo";
+import React from "react";
+
+const page = () => {
+  return <UpdateStaffInfo />;
+};
+
+export default page;
