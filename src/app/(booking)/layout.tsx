468f9b718848bdc06ed98d9315dc4c56f7b662c2
Tkhangds <gunnyvippro111@gmail.com>
2024-11-26 23:19:54 +0700

    feat: implement booking detail and contract API routes with CRUD operations refactor: update BookingLayout to improve ProgressBar visibility and layout style: enhance Select component layout for better responsiveness feat: add Pagination component for improved item navigation

--- src/app/(booking)/layout.tsx
+++ src/app/(booking)/layout.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ProgressBar from "@/components/progressbar/ProgressBar";
 const BookingLayout = (props: { children: React.ReactNode }) => {
   return (
-    <div className="overflow-x-hidden">
-      <ProgressBar />
-      {props.children}
+    <div className="flex flex-col">
+      <div className="max-sm:hidden sticky top-0">
+        <ProgressBar />
+      </div>
+      <div>{props.children}</div>
+      <div className="sm:hidden fixed bottom-0 w-full">
+        <ProgressBar />
+      </div>
     </div>
   );
 };
--- src/components/chart/DropDown.tsx
+++ src/components/chart/DropDown.tsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
+import React, { useState } from "react";
+import Image from "next/image";
 
-const Dropdown = () => {
+type DropdownProps = {
+  setFilter: (filter: string) => void;
+};
+const filterOption = ["Descending", "Ascending", "None"];
+
+const Dropdown: React.FC<DropdownProps> = ({ setFilter }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -10,24 +15,35 @@ const Dropdown = () => {
 
   return (
     <div className="relative">
-        <button className="w-[104px] h-[38px] bg-[#fcfcfc] rounded-lg border border-neutral-300 flex items-center justify-between px-4 hover:bg-gray-400 duration-300" onClick={toggleDropdown}>
-            <div className='text-[#2b3034] text-[13px] font-black leading-[10px] font-gilroy-regular'>
-            Filter By
-            </div>
-            <Image src='/images/Chart/drop_down.svg' alt='drop_down' width={10} height={10}/>
-        </button>
-        {isOpen && <div className="fixed inset-0" onClick={toggleDropdown}></div>}
+      <button
+        className="w-[104px] h-[38px] bg-[#fcfcfc] rounded-lg border border-neutral-300 flex items-center justify-between px-4 hover:bg-gray-400 duration-300"
+        onClick={toggleDropdown}
+      >
+        <div className="text-[#2b3034] text-[13px] font-black leading-[10px] font-gilroy-regular">
+          Filter By
+        </div>
+        <Image
+          src="/images/Chart/drop_down.svg"
+          alt="drop_down"
+          width={10}
+          height={10}
+        />
+      </button>
+      {isOpen && <div className="fixed inset-0" onClick={toggleDropdown}></div>}
       {isOpen && (
-        <div className="origin-top-right absolute right-0 mt-2 w-[104px] rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-            <button className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-400 hover:bg-opacity-30 hover:rounded-t-md duration-300 w-full text-left">
-              Descending
+        <div className="origin-top-right absolute right-0 mt-2 w-[104px] shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+          {filterOption.map((option) => (
+            <button
+              key={option}
+              onClick={() => {
+                setFilter(option);
+                toggleDropdown();
+              }}
+              className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-400 hover:bg-opacity-30 duration-300 w-full text-left"
+            >
+              {option}
             </button>
-            <button className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-400 hover:bg-opacity-30 duration-300 w-full text-left">
-              Ascending
-            </button>
-            <button className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-400 hover:bg-opacity-30 hover:rounded-b-md duration-300 w-full text-left">
-              None
-            </button>
+          ))}
         </div>
       )}
     </div>
