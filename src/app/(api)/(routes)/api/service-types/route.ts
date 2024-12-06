39a9e0bb0d020c065218287e464f2ef30e1d544d
nerkartran297 <nerkar.tran@gmail.com>
2024-12-06 12:56:51 +0700

    Fixed bug for API dashboard/calendar

--- src/app/(api)/(routes)/api/service-types/route.ts
+++ src/app/(api)/(routes)/api/service-types/route.ts
@@ -1,60 +1,59 @@
 import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 import { ServiceTypeSchema } from "./service-types.schema";
-import z from 'zod';
+import z from "zod";
 import { Prisma } from "@prisma/client";
 
 export async function GET() {
     try {
-      const services = await prisma.serviceType.findMany();
-      return NextResponse.json(services);
+        const services = await prisma.serviceType.findMany();
+        return NextResponse.json(services);
     } catch (error) {
-      console.error('Error fetching services:', error);
-      return NextResponse.json(
-        { error: 'Failed to fetch services' },
-        { status: 500 }
-      );
+        console.error("Error fetching services:", error);
+        return NextResponse.json(
+            { error: "Failed to fetch services" },
+            { status: 500 }
+        );
     }
-  }
+}
 
 export async function POST(request: Request) {
-  
     try {
-      const data = await request.json();
-      const parsedData = ServiceTypeSchema.parse(data)
-      const serviceType = await prisma.serviceType.create({
-        data: {
-            categoryId: parsedData.categoryId,
-          name: parsedData.name,
-          description: parsedData.description,
-          basePrice: parsedData.basePrice,
-          isActive: true,
-        },
-      });
-      
-      return NextResponse.json(serviceType);
+        const data = await request.json();
+        const parsedData = ServiceTypeSchema.parse(data);
+        const serviceType = await prisma.serviceType.create({
+            data: {
+                categoryId: parsedData.categoryId,
+                name: parsedData.name,
+                description: parsedData.description,
+                basePrice: parsedData.basePrice,
+                isActive: true,
+            },
+        });
+
+        return NextResponse.json(serviceType);
     } catch (error) {
-      console.error('Error creating service:', error);
-      
-      if (error instanceof z.ZodError) {
-        return NextResponse.json(
-          { error: "Invalid input data", details: error.errors },
-          { status: 400 }
-        );
-      }
+        console.error("Error creating service:", error);
 
-      if (error instanceof Prisma.PrismaClientKnownRequestError) {
-        if (error.code === 'P2003') {
-          return NextResponse.json(
-            { error: `Category id  not found.` },
-            { status: 404 }
-          );
+        if (error instanceof z.ZodError) {
+            return NextResponse.json(
+                { error: "Invalid input data", details: error.errors },
+                { status: 400 }
+            );
         }
-      }
 
-      return NextResponse.json(
-        { error: 'Failed to create service' },
-        { status: 500 }
-      );
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2003") {
+                return NextResponse.json(
+                    { error: `Category id  not found.` },
+                    { status: 404 }
+                );
+            }
+        }
+
+        return NextResponse.json(
+            { error: "Failed to create service" },
+            { status: 500 }
+        );
     }
-  }
+}
