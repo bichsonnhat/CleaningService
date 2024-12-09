ba7dd0e98bc09c52fc5b84ff901e1a659e92bb2d
huytt268 <tuanhuytruong35@gmail.com>
2024-12-09 09:21:40 +0700

    feat: implement leave request functionality with API integration; add helper availability schema and filter component

--- src/app/(api)/(routes)/api/helper_availability/[id]/route.ts
+++ src/app/(api)/(routes)/api/helper_availability/[id]/route.ts
@@ -3,35 +3,48 @@ import { NextResponse } from "next/server";
 
 // Get helper_availability by id
 export async function GET(
-    req: Request,
-    { params }: { params: { id: string } }
+  req: Request,
+  { params }: { params: { id: string } }
 ) {
-    try {
-        const helperAvailability = await prisma.helperAvailability.findUnique({
-            where: {
-                id: params.id,
+  try {
+    const helperAvailability = await prisma.helperAvailability.findUnique({
+      where: {
+        id: params.id,
+      },
+      include: {
+        approvedBy: {
+          select: {
+            fullName: true,
+          },
+        },
+        helper: {
+          select: {
+            user: {
+              select: {
+                fullName: true,
+              },
             },
-        });
-        if (!helperAvailability) {
-            return NextResponse.json(
-                { error: 'User not found' },
-                { status: 404 }
-            );
-        }
-        return NextResponse.json(helperAvailability);
-    } catch (error) {
-        console.error('Error fetching user:', error);
-        return NextResponse.json(
-            { error: 'Failed to fetch user' },
-            { status: 500 }
-        );
+          },
+        },
+      },
+    });
+    if (!helperAvailability) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
+    return NextResponse.json(helperAvailability);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch user" },
+      { status: 500 }
+    );
+  }
 }
 
 // Update helper_availability by id
-export async function PUT(
-    req: Request,
-    { params }: { params: { id: string } }
+export async function PATCH(
+  req: Request,
+  { params }: { params: { id: string } }
 ) {
   const data = await req.json();
   const updatedServiceCategory = await prisma.helperAvailability.update({
@@ -39,27 +52,30 @@ export async function PUT(
       id: params.id,
     },
     data,
+    include: {
+      approvedBy: true,
+    },
   });
   return NextResponse.json(updatedServiceCategory);
 }
 
 // DELETE helper_availability by id
 export async function DELETE(
-    req: Request,
-    { params }: { params: { id: string } }
+  req: Request,
+  { params }: { params: { id: string } }
 ) {
-    try {
-        const helperAvailability = await prisma.helperAvailability.delete({
-            where: {
-                id: params.id,
-            },
-        });
-        return NextResponse.json(helperAvailability);
-    } catch (error) {
-        console.error('Error deleting user:', error);
-        return NextResponse.json(
-            { error: 'Failed to delete user' },
-            { status: 500 }
-        );
-    }
+  try {
+    const helperAvailability = await prisma.helperAvailability.delete({
+      where: {
+        id: params.id,
+      },
+    });
+    return NextResponse.json(helperAvailability);
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    return NextResponse.json(
+      { error: "Failed to delete user" },
+      { status: 500 }
+    );
+  }
 }
--- src/app/(api)/(routes)/api/helper_availability/route.ts
+++ src/app/(api)/(routes)/api/helper_availability/route.ts
@@ -1,17 +1,83 @@
 import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
+import { helperAvailabilityData } from "../../../../../../prisma/mockData";
+import { helperAvailabilitySchema } from "./helper_availability.shema";
 
 // Get all helper_availability
-export async function GET() {
-  const helperAvailability = await prisma.helperAvailability.findMany();
-  return NextResponse.json(helperAvailability);
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const helperId = url.searchParams.get("helperId");
+
+  try {
+    let leaveRequests;
+
+    if (helperId) {
+      leaveRequests = await prisma.helperAvailability.findMany({
+        where: {
+          helperId: helperId,
+        },
+        include: {
+          helper: {
+            select: {
+              user: {
+                select: {
+                  fullName: true,
+                },
+              },
+            },
+          },
+        },
+      });
+    } else {
+      leaveRequests = await prisma.helperAvailability.findMany({
+        include: {
+          helper: {
+            select: {
+              user: {
+                select: {
+                  fullName: true,
+                },
+              },
+            },
+          },
+        },
+      });
+    }
+
+    return NextResponse.json(leaveRequests);
+  } catch (error) {
+    console.error("Error fetching leave requests", error);
+    return NextResponse.json(
+      { status: "error", error: "Failed to fetch leave requests" },
+      { status: 500 } //
+    );
+  }
 }
 
 // Create a new helper_availability
 export async function POST(req: Request) {
-  const data = await req.json();
-  const newHelperAvailability = await prisma.helperAvailability.create({
-    data,
-  });
-  return NextResponse.json(newHelperAvailability);
+  try {
+    const body = await req.json();
+    //Get current User, tam thoi xai User cung
+    const userId = "0066dc01-cdd4-4243-9f4e-778bcfa4458f";
+
+    const data = helperAvailabilitySchema.parse(body);
+
+    const newHelperAvailability = await prisma.helperAvailability.create({
+      data: {
+        helperId: userId,
+        availabilityType: data.availabilityType,
+        startDatetime: data.startDatetime,
+        endDatetime: data.endDatetime,
+        requestReason: data.requestReason,
+      },
+    });
+    return NextResponse.json(newHelperAvailability);
+  } catch (error) {
+    console.error("Error creating leave request:", error);
+    return NextResponse.json(
+      { status: "error", error: "Failed to create leave request" },
+      { status: 500 }
+    );
+  }
 }
