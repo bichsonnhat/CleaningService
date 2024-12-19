2aa968431a02b5688a34fe4e7713bddf861084b1
Bich Son Nhat <55039261+bichsonnhat@users.noreply.github.com>
2024-12-19 23:20:48 +0700

    feat: implement Stripe webhook handler for payment events

---
+++ src/app/(api)/(routes)/api/stripe-webhook/route.ts
@@ -0,0 +1,34 @@
+import { stripe } from '@/lib/stripe';
+import { NextResponse } from 'next/server';
+import Stripe from 'stripe';
+
+
+export async function POST(request: Request) {
+    console.log('Stripe webhook received');
+    const sig = request.headers.get('stripe-signature') || '';
+    const body = await request.text();
+
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent
+            (body, sig, "whsec_b2ddf5804ddf737be87c170d775aa3d364afd8e643cd154e6ef6b1c785fd9edb");
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 400 });
+    }
+    
+    // Handle the event
+    switch (event.type) {
+        case 'payment_intent.succeeded':
+            const paymentIntent = event.data.object;
+            console.log('PaymentIntent was successful!');
+            // Then define and call a method to handle the successful payment intent.
+            // handlePaymentIntentSucceeded(paymentIntent);
+            break;
+        // ... handle other event types
+        default:
+            console.log(`Unhandled event type ${event.type}`);
+    }
+
+    return NextResponse.json({ received: true });
+}
